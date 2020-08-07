c2728ba052907602b7355201f1946f0292166dc1
pedroksty <pedrotrab2132@gmail.com>
2020-08-07 02:42:31 -0300

    Add Redirect routes on login

---
+++ packages/web/src/pages/Dashboad/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Dashboad: React.FC = () => {
+  return <h1>Dashboardwadwad</h1>;
+};
+
+export default Dashboad;
---
+++ packages/web/src/routes/Route.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  RouteProps as ReactDOMRouteProps,
+  Route as ReactDomRoute,
+  Redirect
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+const Routes: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  console.log(user, isPrivate);
+
+  return (
+    <ReactDomRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : 'dashboard',
+              state: { from: location }
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Routes;
