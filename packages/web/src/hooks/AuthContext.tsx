44cf24fbabb434008f403be45f9261e86bc0ebfb
pedroksty <pedrotrab2132@gmail.com>
2020-08-06 23:13:22 -0300

    Add SignOut

--- packages/web/src/hooks/AuthContext.tsx
+++ packages/web/src/hooks/AuthContext.tsx
@@ -14,6 +14,7 @@ interface SignInCrendentials {
 interface AuthContextData {
   user: object;
   signIn(crendentials: SignInCrendentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -44,8 +45,15 @@ export const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
