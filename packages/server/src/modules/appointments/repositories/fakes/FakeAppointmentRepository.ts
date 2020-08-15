08c4e16204da8886b924e2ea2d6db1cd0492f3eb
pedroksty <pedrotrab2132@gmail.com>
2020-08-14 23:21:46 -0300

    Create findBydate in fake Appointment repository

--- packages/server/src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
+++ packages/server/src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
@@ -1,34 +1,35 @@
-import { uuid } from 'uuidv4'
-
-
+import { uuid } from 'uuidv4';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/IcreateAppointmentDTO';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 class AppointmentRepository implements IAppointmentsRepository {
-  private appointments = Appointment[] = []
+  private appointments: Appointment[] = [];
 
- 
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date
+    );
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {}
+    return findAppointment;
+  }
 
   public async create({
     date,
     provider_id
   }: ICreateAppointmentDTO): Promise<Appointment> {
-    const appointment = new Appointment()
+    const appointment = new Appointment();
 
-    Object.assign(appointment, {id: uuid(), date, provider_id})
+    Object.assign(appointment, { id: uuid(), date, provider_id });
 
-    appointment.id = uuid()
-    appointment.date = date
-    appointment.provider_id = provider_id
+    appointment.id = uuid();
+    appointment.date = date;
+    appointment.provider_id = provider_id;
 
-    this.appointments.push(appointment)
+    this.appointments.push(appointment);
 
-    return appointment
-
+    return appointment;
   }
 }
 
