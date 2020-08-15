ea87d9d238c5df66a8745f1788e189d9df734a06
pedroksty <pedrotrab2132@gmail.com>
2020-08-15 00:07:06 -0300

    Create AppointmentService test to de same date

--- packages/server/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ packages/server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,6 @@
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentService from './CreateAppointmentService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -16,5 +17,27 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('1232321');
+  });
+
+  it('should not to able to create two appointments on the same date', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository
+    );
+
+    const appointmentDate = new Date(2020, 5, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '1232321'
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '1232321'
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
