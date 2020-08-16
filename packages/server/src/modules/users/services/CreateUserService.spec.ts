a50b0570db3619e12b283f3d8d940bbf200173ee
pedroksty <pedrotrab2132@gmail.com>
2020-08-15 22:17:44 -0300

    Create user with the same email test

--- packages/server/src/modules/users/services/CreateUserService.spec.ts
+++ packages/server/src/modules/users/services/CreateUserService.spec.ts
@@ -1,5 +1,6 @@
 import FakeUserRepository from '@modules/users/repositories/fakes/FakeUserRepository';
 import CreateUserService from './CreateUserService';
+import AppError from '@shared/errors/AppError';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
@@ -14,5 +15,25 @@ describe('CreateUser', () => {
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with the same email from another', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    await createUser.execute({
+      name: 'Mario Prato',
+      email: 'mario@contact.com',
+      password: '12345678'
+    });
+
+    expect(
+      createUser.execute({
+        name: 'Mario Prato',
+        email: 'mario@contact.com',
+        password: '12345678'
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
