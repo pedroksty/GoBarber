79614d10b8d88af5b7c2b8db87f8527b4b333874
pedroksty <pedrotrab2132@gmail.com>
2020-08-16 03:22:28 -0300

    Create SendForgotPasswordEmailService test

--- packages/server/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ packages/server/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,15 +1,29 @@
 import FakeUserRepository from '@modules/users/repositories/fakes/FakeUserRepository';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
-import AppError from '@shared/errors/AppError';
+import FakeMailProvider from '@shared/container/providers/MailProviver/fakes/FakeMailProvider';
 
 describe('sendForgotPasswordEmail', () => {
   it('should be able to recover the password using the email', async () => {
     const fakeUserRepository = new FakeUserRepository();
+    const fakeMailProvider = new FakeMailProvider();
 
-    const createUser = new SendForgotPasswordEmailService(fakeUserRepository);
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
 
-    const user = await createUser.execute({});
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUserRepository,
+      fakeMailProvider
+    );
 
-    expect(user).toHaveProperty('id');
+    await fakeUserRepository.create({
+      name: 'Mario Prato',
+      email: 'mario@contac.com',
+      password: '12345678'
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'mario@contac.com'
+    });
+
+    expect(sendMail).toHaveBeenCalled();
   });
 });
--- packages/server/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ packages/server/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,6 +5,7 @@ import User from '../infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '../repositories/IUsersREpository';
+import IMailProvider from '@shared/container/providers/MailProviver/models/IMailProvider';
 
 interface IRequest {
   email: string;
@@ -14,10 +15,15 @@ interface IRequest {
 class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvder: IMailProvider
   ) {}
 
-  public async execute(): Promise<void> {}
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvder.sendMail(email, 'Pedido de recuperação de senha recebido');
+  }
 }
 
 export default SendForgotPasswordEmailService;
