e8e26e889f5396fdf6e9e8cfda58e15dd62c8736
pedroksty <pedrotrab2132@gmail.com>
2020-08-18 05:46:54 -0300

    Refactor

--- packages/server/src/shared/infra/http/app.ts
+++ packages/server/src/shared/infra/http/app.ts
@@ -1,24 +1,24 @@
-import 'reflect-metadata';
+import 'reflect-metadata'
 
-import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import 'express-async-errors';
+import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
+import 'express-async-errors'
 
-import router from './routes';
-import AppError from '@shared/errors/AppError';
-import uploadConfig from '@config/upload';
+import router from './routes'
+import AppError from '@shared/errors/AppError'
+import uploadConfig from '@config/upload'
 
-import '@shared/infra/typeorm';
-import '@shared/container';
+import '@shared/infra/typeorm'
+import '@shared/container'
 
-const app = express();
+export const port = 4444
 
-export const port = 4444;
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use('/files', express.static(uploadConfig.uploadsFolder));
-app.use(router);
+app.use(express.json())
+app.use(cors())
+app.use('/files', express.static(uploadConfig.uploadsFolder))
+app.use(router)
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
@@ -26,16 +26,16 @@ app.use(
       return response.status(err.statusCode).json({
         status: 'error',
         message: err.message
-      });
+      })
     }
 
-    console.error(err);
+    console.error(err)
 
     return response.status(500).json({
       status: 'error',
       message: 'Internal server error'
-    });
+    })
   }
-);
+)
 
-export default app;
+export default app
