6ad2f95a5823b05b16207c967a88f4214932fe31
pedroksty <pedrotrab2132@gmail.com>
2020-08-18 02:18:02 -0300

    Refactor

--- packages/server/src/modules/users/repositories/fakes/FakeUserRepository.ts
+++ packages/server/src/modules/users/repositories/fakes/FakeUserRepository.ts
@@ -1,41 +1,41 @@
-import { uuid } from 'uuidv4';
+import { uuid } from 'uuidv4'
 
-import IUsersRepository from '@modules/users/repositories/IUsersREpository';
-import IcreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IUsersRepository from '@modules/users/repositories/IUsersREpository'
+import IcreateUserDTO from '@modules/users/dtos/ICreateUserDTO'
 
-import User from '@modules/users/infra/typeorm/entities/User';
+import User from '@modules/users/infra/typeorm/entities/User'
 
 class FakeUsersRepository implements IUsersRepository {
-  private users: User[] = [];
+  private users: User[] = []
   public async findById(id: string): Promise<User | undefined> {
-    const findUser = this.users.find(user => user.id === id);
+    const findUser = this.users.find(user => user.id === id)
 
-    return findUser;
+    return findUser
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const findUser = this.users.find(user => user.email === email);
+    const findUser = this.users.find(user => user.email === email)
 
-    return findUser;
+    return findUser
   }
 
   public async create(userData: IcreateUserDTO): Promise<User> {
-    const user = new User();
+    const user = new User()
 
-    Object.assign(user, { id: uuid() }, userData);
+    Object.assign(user, { id: uuid() }, userData)
 
-    this.users.push(user);
+    this.users.push(user)
 
-    return user;
+    return user
   }
 
   public async save(user: User): Promise<User> {
-    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id)
 
-    this.users[findIndex] = user;
+    this.users[findIndex] = user
 
-    return user;
+    return user
   }
 }
 
-export default FakeUsersRepository;
+export default FakeUsersRepository
--- packages/server/src/modules/users/repositories/fakes/FakeUsersTokenRepository.ts
+++ packages/server/src/modules/users/repositories/fakes/FakeUsersTokenRepository.ts
@@ -1,14 +1,14 @@
-import { uuid } from 'uuidv4';
+import { uuid } from 'uuidv4'
 
-import IUserTokenRepository from '../IUsersTokensRepository';
+import IUserTokenRepository from '../IUsersTokensRepository'
 
-import UserToken from '../../infra/typeorm/entities/UserToken';
+import UserToken from '../../infra/typeorm/entities/UserToken'
 
 class FakeUserTokenRepository implements IUserTokenRepository {
-  private userToken: UserToken[] = [];
+  private userToken: UserToken[] = []
 
   public async generate(user_id: string): Promise<UserToken> {
-    const userToken = new UserToken();
+    const userToken = new UserToken()
 
     Object.assign(userToken, {
       id: uuid(),
@@ -16,20 +16,20 @@ class FakeUserTokenRepository implements IUserTokenRepository {
       user_id,
       created_at: new Date(),
       update: new Date()
-    });
+    })
 
-    this.userToken.push(userToken);
+    this.userToken.push(userToken)
 
-    return userToken;
+    return userToken
   }
 
   public async findByToken(token: string): Promise<UserToken | undefined> {
     const userToken = this.userToken.find(
       findToken => findToken.token === token
-    );
+    )
 
-    return userToken;
+    return userToken
   }
 }
 
-export default FakeUserTokenRepository;
+export default FakeUserTokenRepository
