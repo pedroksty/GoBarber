e16c1a983917e1f812a563978808d5d8106c6ad3
pedroksty <pedrotrab2132@gmail.com>
2020-08-21 04:25:16 -0300

    Add cache to ListProviderAppointemtsService

--- packages/server/src/modules/appointments/services/CreateAppointmentService.ts
+++ packages/server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -5,6 +5,7 @@ import AppError from '@shared/errors/AppError'
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 import INotificationRepository from '@modules/notifications/repositories/INotificationRepository'
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 
 interface IRequest {
   date: Date
@@ -19,7 +20,10 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepository,
 
     @inject('NotificationRepository')
-    private notificationsRepository: INotificationRepository
+    private notificationsRepository: INotificationRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({
@@ -61,6 +65,13 @@ class CreateAppointmentService {
       recipient_id: provider_id,
       content: `Novo Agendamento para dia ${dateFormatted}`
     })
+
+    await this.cacheProvider.invalidate(
+      `provider-appointments:${provider_id}:${format(
+        appointmentDate,
+        'yyyy-M-d'
+      )}`
+    )
 
     return appointment
   }
--- packages/server/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ packages/server/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,20 +27,22 @@ class ListProviderAppointmentsService {
     year,
     month
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('asd')
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`
 
-    console.log(cacheData)
+    let appointments = await this.cacheProvider.recover<Appointment[]>(cacheKey)
 
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-      {
-        provider_id,
-        day,
-        year,
-        month
-      }
-    )
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          day,
+          year,
+          month
+        }
+      )
 
-    // await this.cacheProvider.save('asd', 'asdw')
+      await this.cacheProvider.save(cacheKey, appointments)
+    }
 
     return appointments
   }
