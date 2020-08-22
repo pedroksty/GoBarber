7294a8ea301c318feed7b3bbcf26468cd9b427f7
pedroksty <pedrotrab2132@gmail.com>
2020-08-22 03:25:26 -0300

    Create Reset Password page

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -5,7 +5,9 @@ import Route from './Route'
 
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
+
 import ForgotPassword from '../pages/ForgotPassword'
+import ResetPassword from '../pages/ResetPassword'
 
 import Dashboad from '../pages/Dashboad'
 
@@ -15,6 +17,7 @@ const Routes: React.FC = () => {
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" component={SignUp} />
       <Route path="/forgot-password" component={ForgotPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
 
       <Route path="/dashboard" component={Dashboad} isPrivate />
     </Switch>
